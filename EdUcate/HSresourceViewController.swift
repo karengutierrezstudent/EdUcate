8c31aec4254885bb1fae0496fa58818bca33bed5
Karen Gutierrez <GutierrezKaren1007@gmail.com>
2020-07-30 11:12:23 -0700

    commit second files

---
+++ EdUcate/HSresourceViewController.swift
@@ -0,0 +1,30 @@
+//
+//  HSresourceViewController.swift
+//  EdUcate
+//
+//  Created by Karen Gutierrez  on 7/30/20.
+//  Copyright © 2020 EYKR. All rights reserved.
+//
+
+import UIKit
+
+class HSresourceViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
